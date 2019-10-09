33f083f82a3632f19b7ec54adbc32b13b6c0d9c8
Oleksandr Gevko <2190272@c2-lcm-m05.local>
2019-10-09 20:22:21 +0100

    first commit

---
+++ IUE-Worksheet2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IUE-Worksheet2
+//
+//  Created by Marco Oliveira on 02/10/2019.
+//  Copyright © 2019 Marco Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
